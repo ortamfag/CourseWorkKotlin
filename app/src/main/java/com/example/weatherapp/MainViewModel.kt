c9f81383da1dd5b68ec18c82453696c89fe11069
ortamfag <a_zhidov@outlook.com>
2023-01-15 17:01:19 +0300

    ADD: change layout

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -4,8 +4,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weatherapp.adapters.WeatherModel
 
-//const val API_KEY = "cfd3109701ae4f0691e123848230401"
-
 class MainViewModel : ViewModel() {
     val liveDataCurrent = MutableLiveData<WeatherModel>()
     val liveDataList = MutableLiveData<List<WeatherModel>>()
